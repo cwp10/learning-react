ef903c4b3d276c76aeb0ee872e7fb99caa17e3c5
Won Pyoung Choi <cwp10@msn.com>
2018-01-15 22:03:07 +0900

    3-2 까지 공부

---
+++ ch3/form_simple/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {SimpleForm} from './SimpleForm'
+import './App.css';
+
+// 메인 화면 컴포넌트 입니다.
+export default class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <SimpleForm />
+      </div>
+    )
+  }
+}
